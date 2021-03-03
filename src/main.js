a74b06a9b555a8779af2cf3239ccc4e808268d20
kang <kangvangent@gmail.com>
2021-03-03 14:53:54 +0700

    build&hosting-project

--- src/main.js
+++ src/main.js
@@ -1,32 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import jQuery from 'jquery'
-import axios from 'axios'
-import firebase from 'firebase/app'
-global.jQuery = jQuery
-global.$ = jQuery
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import jQuery from "jquery";
+import axios from "axios";
+import firebase from "firebase/app";
+global.jQuery = jQuery;
+global.$ = jQuery;
 
-Vue.prototype.$axios = axios
-Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+Vue.config.productionTip = false;
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBdJYPHvhaFuT1i2DyAlnuNOdSWHZCcaj4',
-  authDomain: 'govr-42c7d.firebaseapp.com',
-  projectId: 'govr-42c7d',
-  storageBucket: 'govr-42c7d.appspot.com',
-  messagingSenderId: '687841800798',
-  appId: '1:687841800798:web:5f83a9ecdfd8d7b2503acf',
-  measurementId: 'G-ZJQ4Q76M37'
-}
+  apiKey: "AIzaSyBdJYPHvhaFuT1i2DyAlnuNOdSWHZCcaj4",
+  authDomain: "govr-42c7d.firebaseapp.com",
+  projectId: "govr-42c7d",
+  storageBucket: "govr-42c7d.appspot.com",
+  messagingSenderId: "687841800798",
+  appId: "1:687841800798:web:5f83a9ecdfd8d7b2503acf",
+  measurementId: "G-ZJQ4Q76M37",
+};
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-firebase.analytics()
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Profile from '../views/Profile.vue'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Profile from "../views/Profile.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/profile',
-    name: 'Profile',
-    component: Profile
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  }
-]
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
